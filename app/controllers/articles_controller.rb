d04da8f9f1abd0ea83445c76c1fbac2444e6b5b7
Mohamed Masoud <joeblack_8@hotmail.com>
2016-01-16 19:17:46 +0300

    styling the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
-
-	def new
-	  @article = Article.new
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+	
+	def index
+		@articles = Article.all
 	end
-
+	
+	def new
+		@article = Article.new
+	end
+	
+	def edit
+	end
+	
 	def create
 		@article = Article.new(article_params)
 		if @article.save
@@ -14,14 +22,30 @@ class ArticlesController < ApplicationController
 		end
 	end
 	
+	def update
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+	
 	def show
-		@article = Article.find(params[:id])
 	end
-
+	
+	def destroy
+		@article.destroy
+		flash[:notice] = "Article was successfully deleted"
+		redirect_to articles_path
+	end
+	
 	private
-		def article_params
-			params.require(:article).permit(:title, :description)
+		def set_article
+			@article = Article.find(params[:id])
+		end
+	
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
-
-
 end
